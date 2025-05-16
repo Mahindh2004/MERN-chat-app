73cab9909e978b718ef91a256f68c3b8dd7c4a9f
mahindh2004 <mahindhsuriyakumar2004@gmail.com>
2025-05-16 12:29:06 +0530

    chat

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getMessages, getUsersforSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
 const messageRouter = express.Router();
-messageRouter.get("/users",protectRoute,getUsersforSidebar);
+messageRouter.get("/users",protectRoute,getUsersForSidebar);
 messageRouter.get("/:id",protectRoute,getMessages);
 messageRouter.put("mark/:id",protectRoute,markMessageAsSeen);
 messageRouter.post("/send/:id",protectRoute,sendMessage);
