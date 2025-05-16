884748e7bb4c8f73705b4cc4681f9b680cbff113
mahindh2004 <mahindhsuriyakumar2004@gmail.com>
2025-05-16 12:00:45 +0530

    first commit

---
+++ server/lib/cloudinary.js
@@ -0,0 +1,7 @@
+import { v2 as cloudinary} from "cloudinary";
+cloudinary.config({
+    cloud_name: 'dgwpngekd',
+    api_key: '579575386661339',
+    api_secret: 'ArlfVvlrstqoq8cK4qAis_9t1TY',
+});
+export default cloudinary;
